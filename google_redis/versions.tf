34521bfb5e79e5701f9537467598a1311ede9fc8
astults <astults@mozilla.com>
2022-02-07 14:05:51 -0700

    Changing this to minimum versions in module providers

--- google_redis/versions.tf
+++ google_redis/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = ">= 3.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.0"
+      version = ">= 4.0"
     }
   }
   required_version = "~> 1.0.0"
