6e9a8c90cdcad9df653e2a6a1a85049346d61830
Sven Marnach <sven@mozilla.com>
2022-06-08 18:16:54 +0200

    Add module for Github Actions deployment service accounts.

---
+++ google_deployment_accounts/main.tf
@@ -0,0 +1,18 @@
+/**
+ * # Terraform Module: Service Accounts for deployment from GitHub Actions
+ * Creates a Cloud IAM service accounts which let GitHub Actions workflows authenticate to GKE.
+ */
+
+resource "google_service_account" "account" {
+  account_id   = coalesce(var.account_id, "deploy-${var.environment}")
+  display_name = "Deployment to the ${var.environment} environment"
+  project      = var.project
+}
+
+resource "google_service_account_iam_binding" "github-actions-access" {
+  service_account_id = google_service_account.account.name
+  role               = "roles/iam.workloadIdentityUser"
+  members = [
+    "principal://iam.googleapis.com/projects/${var.wip_project_number}/locations/global/workloadIdentityPools/${var.wip_name}/subject/repo:${github_repository}:environment:${var.environment}",
+  ]
+}
---
+++ google_deployment_accounts/variables.tf
@@ -0,0 +1,31 @@
+variable "account_id" {
+  type        = string
+  description = "Name of the service account. Defaults to deploy-<env>"
+  default     = null
+}
+
+variable "environment" {
+  description = "Environment e.g., stage."
+  type        = string
+}
+
+variable "project" {
+  type    = string
+  default = null
+}
+
+variable "wip_project_number" {
+  type        = number
+  description = "The project number of the project the workload identity provider lives in"
+}
+
+variable "wip_name" {
+  type        = string
+  description = "The name of the workload identity provider"
+  default     = "github-actions"
+}
+
+variable "github_repository" {
+  type        = string
+  description = "The Github repository running the deployment workflows in the format org/repository"
+}
