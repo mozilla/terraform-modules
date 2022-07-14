b8c123d44b49fee09093480c525a4f9d6e1671d3
Chris Valaas <cvalaas@mozilla.com>
2022-07-14 10:50:51 -0700

    fixing project var

--- google_gsm_for_gke/gke_service_account.tf
+++ google_gsm_for_gke/gke_service_account.tf
@@ -1,11 +1,11 @@
 resource "google_service_account" "gke-account" {
   account_id  = "gke-${var.environment}"
   description = "GKE deployments in the ${var.environment} environment"
-  project     = var.project
+  project     = var.project_id
 }
 
 resource "google_service_account_iam_binding" "workload-identity-for-gke" {
-  project            = var.project
+  project            = var.project_id
   service_account_id = google_service_account.gke-account.name
   role               = "roles/iam.workloadIdentityUser"
   members = [
@@ -16,19 +16,19 @@ resource "google_service_account_iam_binding" "workload-identity-for-gke" {
 # permissions for use with External Secrets Operator in GKE
 #
 resource "google_project_iam_member" "sa-role-token-creator" {
-  project = var.project
+  project = var.project_id
   role    = "roles/iam.serviceAccountTokenCreator"
   member  = "serviceAccount:${google_service_account.gke-account.name}"
 }
 
 resource "google_project_iam_member" "sa-role-secret-viewer" {
-  project = var.project
+  project = var.project_id
   role    = "roles/secretmanager.viewer"
   member  = "serviceAccount:${google_service_account.gke-account.name}"
 }
 
 resource "google_project_iam_member" "sa-role-secret-accessor" {
-  project = var.project
+  project = var.project_id
   role    = "roles/secretmanager.secretAccessor"
   member  = "serviceAccount:${google_service_account.gke-account.name}"
 }
