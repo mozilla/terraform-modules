e314f21df35fa12b722add04b3a512c5e9698a8d
Chris Valaas <cvalaas@mozilla.com>
2022-07-14 10:38:04 -0700

    formatting

--- google_gsm_for_gke/gke_service_account.tf
+++ google_gsm_for_gke/gke_service_account.tf
@@ -1,16 +1,14 @@
 resource "google_service_account" "gke-account" {
-  for_each     = toset( var.environment )
-  account_id   = "gke-${each.key}"
-  description  = "GKE deployments in the ${each.key} environment"
-  project      = var.project
+  account_id  = "gke-${var.environment}"
+  description = "GKE deployments in the ${var.environment} environment"
+  project     = var.project
 }
 
 resource "google_service_account_iam_binding" "workload-identity-for-gke" {
-  for_each           = toset( var.environment )
   project            = var.project
-  service_account_id = google_service_account.gke-account[each.key].name
+  service_account_id = google_service_account.gke-account.name
   role               = "roles/iam.workloadIdentityUser"
-  members            = [
+  members = [
     "serviceAccount:${var.cluster_project_id}.svc.id.goog[${var.namespace}/external-secrets]"
   ]
 }
@@ -18,22 +16,19 @@ resource "google_service_account_iam_binding" "workload-identity-for-gke" {
 # permissions for use with External Secrets Operator in GKE
 #
 resource "google_project_iam_member" "sa-role-token-creator" {
-  for_each = toset( var.environment )
   project  = var.project
   role     = "roles/iam.serviceAccountTokenCreator"
-  member   = "serviceAccount:${google_service_account.gke-account[each.key].name}"
+  member   = "serviceAccount:${google_service_account.gke-account.name}"
 }
 
 resource "google_project_iam_member" "sa-role-secret-viewer" {
-  for_each = toset( var.environment )
   project  = var.project
   role     = "roles/secretmanager.viewer"
-  member   = "serviceAccount:${google_service_account.gke-account[each.key].name}"
+  member   = "serviceAccount:${google_service_account.gke-account.name}"
 }
 
 resource "google_project_iam_member" "sa-role-secret-accessor" {
-  for_each = toset( var.environment )
   project  = var.project
   role     = "roles/secretmanager.secretAccessor"
-  member   = "serviceAccount:${google_service_account.gke-account[each.key].name}"
+  member   = "serviceAccount:${google_service_account.gke-account.name}"
 }
