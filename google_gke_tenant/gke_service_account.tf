d2172f93db52f77630aebc9c93b7ae55dd5d35bd
Chris Valaas <cvalaas@mozilla.com>
2022-07-14 12:00:36 -0700

    adding a comment

--- google_gke_tenant/gke_service_account.tf
+++ google_gke_tenant/gke_service_account.tf
@@ -14,6 +14,10 @@ resource "google_service_account_iam_binding" "workload-identity-for-gke" {
 
 # permissions for use with External Secrets Operator in GKE
 #
+# FIXME: The below permission isn't required if we use the pod-based workload identity
+# https://external-secrets.io/v0.5.8/provider-google-secrets-manager/#using-pod-based-workload-identity
+# but the instructions there are ... vague and I don't understand them.
+#
 resource "google_project_iam_member" "sa-role-token-creator" {
   project = var.project_id
   role    = "roles/iam.serviceAccountTokenCreator"
