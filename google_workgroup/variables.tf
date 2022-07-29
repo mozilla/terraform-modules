7386439ccbb001f5dea1f2dc3d8d973e19f6cf38
Jason Thomas <jason@lithiumfox.com>
2022-07-29 15:46:22 -0400

    google_workgroup: allow digits in workgroup

--- google_workgroup/variables.tf
+++ google_workgroup/variables.tf
@@ -3,7 +3,7 @@ variable "ids" {
   description = "List of workgroup identifiers to look up access for"
 
   validation {
-    condition     = alltrue([for i in var.ids : length(regexall("^workgroup:[a-zA-Z-]+(/[a-zA-Z0-9\\.-]+)?$|^subgroup:[a-zA-Z0-9\\.-]+$", i)) > 0])
+    condition     = alltrue([for i in var.ids : length(regexall("^workgroup:[a-zA-Z0-9-]+(/[a-zA-Z0-9\\.-]+)?$|^subgroup:[a-zA-Z0-9\\.-]+$", i)) > 0])
     error_message = "Bad workgroup identifier format, must match workgroup:WORKGROUP[/SUBGROUP] or subgroup:SUBGROUP."
   }
 }
