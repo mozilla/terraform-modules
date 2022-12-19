2cacc1f216fac09b0c45e417054c3c4dccb10cda
Dustin Lactin <dlactin@mozilla.com>
2022-12-19 09:56:42 -0700

    Added node_pools_taints to variables

--- google_gke/variables.tf
+++ google_gke/variables.tf
@@ -195,6 +195,14 @@ variable "node_pools_tags" {
   }
 }
 
+variable "node_pools_taints" {
+  description = "Map containing node pools taints. Each node pool's name is the key. See locals.tf for defaults."
+  type        = map(map(map(string)))
+  default = {
+    tf-default-node-pool = {}
+  }
+}
+
 #
 # Monitoring
 #
