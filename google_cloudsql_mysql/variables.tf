f0d85cb64bacc4f58e4124718757a76820aab83b
André Honeiser <andre@honeiser.de>
2023-01-30 11:11:29 +0100

    feat(google_cloudsql_mysql): add region override for replicas (#75)

--- google_cloudsql_mysql/variables.tf
+++ google_cloudsql_mysql/variables.tf
@@ -78,6 +78,11 @@ variable "replica_tier_override" {
   default     = ""
 }
 
+variable "replica_region_override" {
+  description = "This OVERRIDES var.region for replicas (replicas use var.region per default)."
+  default     = ""
+}
+
 variable "instance_version" {
   default     = "v1"
   description = "Version of database. Use this field if you need to spin up a new database instance."
