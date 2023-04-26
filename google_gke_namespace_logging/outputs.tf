087abe0edeb2cd61a2f1775cb6f832d8a88120ac
Dustin Lactin <dlactin@mozilla.com>
2023-04-26 14:17:58 -0600

    Fixed outputs

--- google_gke_namespace_logging/outputs.tf
+++ google_gke_namespace_logging/outputs.tf
@@ -1,7 +1,7 @@
 output "logging_bucket_id" {
-  value = google_logging_project_bucket_config.namespace.id
+  value = google_logging_project_bucket_config[0].namespace.id
 }
 
 output "logging_dataset_id" {
-  value = google_bigquery_dataset.namespace.id
+  value = google_bigquery_dataset.namespace[0].id
 }
