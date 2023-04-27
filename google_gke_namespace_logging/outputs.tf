bf994f00c7dc184bc5860940ce0092cda265a3fb
Dustin Lactin <dustin.lactin@gmail.com>
2023-04-27 13:20:35 -0600

    feat: Support bq tenant logging as an alternative to logging bucket (#107)
    * Added support for tenant bigquery logging
    
    * Fixed outputs
    
    * Added conditional outputs for dataset and log bucket
    
    * Added replace to ensure proper dataset id syntax
    
    * Changed outputs to return first resource found
    
    * Fixed description tenant_namespace var
    
    * Update google_gke_namespace_logging/main.tf
    
    Co-authored-by: Jason Thomas <jason@lithiumfox.com>
    
    * Using google_bigquery_dataset_iam_member resource instead of project_iam_member
    
    * Fixed dataset instance config
    
    ---------
    
    Co-authored-by: Jason Thomas <jason@lithiumfox.com>

--- google_gke_namespace_logging/outputs.tf
+++ google_gke_namespace_logging/outputs.tf
@@ -1,3 +1,7 @@
 output "logging_bucket_id" {
-  value = google_logging_project_bucket_config.namespace.id
+  value = var.log_destination == "bucket" ? google_logging_project_bucket_config.namespace[0].id : null
+}
+
+output "logging_dataset_id" {
+  value = var.log_destination == "bigquery" ? google_bigquery_dataset.namespace[0].id : null
 }
