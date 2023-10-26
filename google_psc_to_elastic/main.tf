5ea5abaf2397e166f06b2e0d5572b27fb388e925
bqbn <bqbn@openken.com>
2023-10-25 17:54:00 -0700

    Terraform formatting

--- google_psc_to_elastic/main.tf
+++ google_psc_to_elastic/main.tf
@@ -16,7 +16,7 @@ data "google_compute_network" "default" {
 }
 
 data "google_compute_subnetwork" "default" {
-  name = var.subnetwork_name
+  name    = var.subnetwork_name
   project = var.project_id_for_network != "" ? var.project_id_for_network : data.google_project.project.project_id
 }
 
