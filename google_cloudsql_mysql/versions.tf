54960b4be678c9a9aee9e795ba65ab6c8d1842f4
Jon Buckley <jon@jbuckley.ca>
2023-12-01 11:09:24 -0500

    fix(google_cloudsql_*): Allow google v5

--- google_cloudsql_mysql/versions.tf
+++ google_cloudsql_mysql/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.48"
+      version = ">= 4.48"
     }
   }
   required_version = ">= 1.0"
