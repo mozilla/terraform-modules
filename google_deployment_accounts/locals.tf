9459e7fb0a6dcc62aab664be037d61c0e03d943e
Robert Müller <rmuller@mozilla.com>
2024-01-31 14:38:23 +0100

    fix fmt
    Signed-off-by: Robert Müller <rmuller@mozilla.com>

--- google_deployment_accounts/locals.tf
+++ google_deployment_accounts/locals.tf
@@ -3,7 +3,7 @@ locals {
 
   environments = length(var.gha_environments) > 0 ? var.gha_environments : [var.environment]
 
-  github_deploy_members = flatten([ for repo in local.github_deploy_repositories : [ for env in local.environments : [
+  github_deploy_members = flatten([for repo in local.github_deploy_repositories : [ for env in local.environments : [
     "principal://iam.googleapis.com/projects/${var.wip_project_number}/locations/global/workloadIdentityPools/${var.wip_name}/subject/repo:${repo}:environment:${env}"
   ]]])
 }
