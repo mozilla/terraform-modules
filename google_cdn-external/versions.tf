4119b2c21d2acd3cc2b5cfcae786e13067bc445a
Jon Buckley <jon@jbuckley.ca>
2024-02-07 11:48:00 -0500

    chore(google_cdn-external): Allow using Google v5 provider

--- google_cdn-external/versions.tf
+++ google_cdn-external/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.42, < 5"
+      version = ">= 4.42"
     }
   }
   required_version = ">= 0.13"
