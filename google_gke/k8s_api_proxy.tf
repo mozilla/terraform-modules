cb4f1e83327530588d77b913a5fb4f44a8366a9c
Dustin Lactin <dustin.lactin@gmail.com>
2024-04-18 09:43:15 -0600

    feat(google_gke): enable internal ip reservation for k8s api proxy (#172)
    * feat(google_gke): enable internal ip reservation for k8s api proxy
    
    * style(google_gke): fixed internal_lb_subnetworks variable fields

---
+++ google_gke/k8s_api_proxy.tf
@@ -0,0 +1,13 @@
+#
+# K8S API Proxy Setup
+#
+resource "google_compute_address" "static_v4_k8s_api_proxy_ip" {
+  count        = var.enable_k8s_api_proxy_ip ? 1 : 0
+  provider     = google-beta # At this time the beta provider is required to define labels
+  name         = format("k8s-api-proxy-%s-%s-ip-v4-internal", var.name, var.region)
+  description  = format("IPv4 Internal - k8s api proxy - name:%s region:%s", var.name, var.region)
+  subnetwork   = var.internal_lb_subnetworks[var.region].subnetwork
+  address_type = "INTERNAL"
+
+  labels = local.labels
+}
