86d389fd83b86aec0f204adb9374939abe9e5dee
Basma1912 <basad@mozilla.com>
2024-06-04 14:19:54 -0400

    add_new_additional_role
    Signed-off-by: Basma1912 <basad@mozilla.com>

--- google_permissions/other_roles.tf
+++ google_permissions/other_roles.tf
@@ -119,3 +119,17 @@ resource "google_project_iam_binding" "nonprod_developer_pubsub_editor" {
   role    = "roles/pubsub.editor"
   members = module.developers_workgroup.members
 }
+
+resource "google_project_iam_binding" "prod_developer_colabEnterpriseUser" {
+  count   = contains(var.prod_roles, "roles/aiplatform.colabEnterpriseUser") && !var.admin_only && var.google_prod_project_id != "" ? 1 : 0
+  project = var.google_prod_project_id
+  role    = "roles/aiplatform.colabEnterpriseUser"
+  members = module.developers_workgroup.members
+}
+
+resource "google_project_iam_binding" "nonprod_developer_colabEnterpriseUser" {
+  count   = contains(var.nonprod_roles, "roles/aiplatform.colabEnterpriseUser") && !var.admin_only && var.google_nonprod_project_id != "" ? 1 : 0
+  project = var.google_nonprod_project_id
+  role    = "roles/aiplatform.colabEnterpriseUser"
+  members = module.developers_workgroup.members
+}
