06cd34dd4aae7e7ddf7b62ecbe1ab2a36e55f3ff
Jon Buckley <jon@jbuckley.ca>
2024-06-26 12:03:42 -0400

    chore(aws_gcp_vpn): Allow using aws and google v5 providers

--- aws_gcp_vpn/versions.tf
+++ aws_gcp_vpn/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.37"
+      version = ">= 4.37"
     }
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.41"
+      version = ">= 4.41"
     }
   }
 }
