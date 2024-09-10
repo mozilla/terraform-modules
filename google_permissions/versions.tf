89bdacc413a214b1f89b3a839891d30644749114
James Francis <931363+tcotav@users.noreply.github.com>
2024-09-10 09:48:36 -0700

    missed the beta for pam

--- google_permissions/versions.tf
+++ google_permissions/versions.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/google"
       version = ">= 3.0"
     }
+
+    google-beta = {
+      source = "hashicorp/google-beta"
+      version = "6.0.1"
+    }
   }
   required_version = "~> 1.2"
 }
