e161e6f335411cbce41ce1b6d474cd4d62b6d4d4
Basma1912 <basad@mozilla.com>
2024-11-04 15:58:55 -0500

    OPST-1385_point_permission_module_to_google_workgroup_module
    Signed-off-by: Basma1912 <basad@mozilla.com>

--- google_permissions/workgroups.tf
+++ google_permissions/workgroups.tf
@@ -1,14 +1,20 @@
 module "admins_workgroup" {
-  source = "../google_workgroup"
-  ids    = var.admin_ids
+  source                        = "../google_workgroup"
+  ids                           = var.admin_ids
+  terraform_remote_state_bucket = "moz-fx-platform-mgmt-global-tf"
+  terraform_remote_state_prefix = "projects/google-workspace-management"
 }
 
 module "developers_workgroup" {
-  source = "../google_workgroup"
-  ids    = var.developer_ids
+  source                        = "../google_workgroup"
+  ids                           = var.developer_ids
+  terraform_remote_state_bucket = "moz-fx-platform-mgmt-global-tf"
+  terraform_remote_state_prefix = "projects/google-workspace-management"
 }
 
 module "viewers_workgroup" {
-  source = "../google_workgroup"
-  ids    = var.viewer_ids
+  source                        = "../google_workgroup"
+  ids                           = var.viewer_ids
+  terraform_remote_state_bucket = "moz-fx-platform-mgmt-global-tf"
+  terraform_remote_state_prefix = "projects/google-workspace-management"
 }
