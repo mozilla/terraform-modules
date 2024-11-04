9710042107cee107051bd52f51187840d3a7b691
Basma1912 <basad@mozilla.com>
2024-11-04 15:39:43 -0500

    OPST-1385_point_permission_module_to_google_workgroup_module
    Signed-off-by: Basma1912 <basad@mozilla.com>

--- google_permissions/workgroups.tf
+++ google_permissions/workgroups.tf
@@ -1,14 +1,14 @@
 module "admins_workgroup" {
-  source = "../mozilla_workgroup"
+  source = "../google_workgroup"
   ids    = var.admin_ids
 }
 
 module "developers_workgroup" {
-  source = "../mozilla_workgroup"
+  source = "../google_workgroup"
   ids    = var.developer_ids
 }
 
 module "viewers_workgroup" {
-  source = "../mozilla_workgroup"
+  source = "../google_workgroup"
   ids    = var.viewer_ids
 }
