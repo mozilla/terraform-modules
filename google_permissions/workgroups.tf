cb5d7d363a3a9374b287930f99842cb786c6b59f
Basma1912 <basad@mozilla.com>
2024-11-04 16:40:51 -0500

    chore(module):google permission update

--- google_permissions/workgroups.tf
+++ google_permissions/workgroups.tf
@@ -1,10 +1,11 @@
 module "admins_workgroup" {
   source                        = "../google_workgroup"
   ids                           = var.admin_ids
+  roles                         = {}
   workgroup_outputs             = ["members", "google_groups"]
   terraform_remote_state_bucket = "moz-fx-platform-mgmt-global-tf"
   terraform_remote_state_prefix = "projects/google-workspace-management"
-  roles                         = {}
+
 }
 
 module "developers_workgroup" {
