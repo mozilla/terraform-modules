0ce2d2fe260b543ebb8af9221baf115baad2fd1e
Brett Kochendorfer <bkochendorfer@mozilla.com>
2025-03-10 09:43:48 -0500

    feat(fastly): Use stub

--- google_fastly_waf/provider.tf
+++ google_fastly_waf/provider.tf
@@ -1,4 +1,6 @@
 # NGWAF SIGSCI
 provider "sigsci" {
+  corp     = "not_a_real_copr"
+  email    = "not_a_real_email"
   password = "not_a_real_password_set_for_terraform_validate" # pragma: allowlist secret
 }
