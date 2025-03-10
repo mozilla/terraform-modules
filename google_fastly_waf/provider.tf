7f6d16d5102a170710b0a8cd7ef433bf71cb47aa
Jon Buckley <jon@jbuckley.ca>
2025-03-10 12:37:12 -0400

    fix(google_fastly_waf): Remove provider configuration (#275)
    The provider is configured via environment variables in Atlantis, so it
    doesn't need placeholder values.

--- google_fastly_waf/provider.tf
+++
@@ -1,6 +0,0 @@
-# NGWAF SIGSCI
-provider "sigsci" {
-  corp     = "not_a_real_corp"
-  email    = "not_a_real_email"
-  password = "not_a_real_password_set_for_terraform_validate" # pragma: allowlist secret
-}
