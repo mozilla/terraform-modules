dbfc096ebcd683b0260ec8ab0117622f2155ac48
Brett Kochendorfer <bkochendorfer@mozilla.com>
2025-03-10 09:45:44 -0500

    Update google_fastly_waf/provider.tf

--- google_fastly_waf/provider.tf
+++ google_fastly_waf/provider.tf
@@ -1,6 +1,6 @@
 # NGWAF SIGSCI
 provider "sigsci" {
-  corp     = "not_a_real_copr"
+  corp     = "not_a_real_corp"
   email    = "not_a_real_email"
   password = "not_a_real_password_set_for_terraform_validate" # pragma: allowlist secret
 }
