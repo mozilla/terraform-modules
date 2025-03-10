e9959e3939de69b27a11f94c138ec9c43ae1ae62
Brett Kochendorfer <bkochendorfer@mozilla.com>
2025-03-10 09:42:40 -0500

    feat: Remove variables

--- google_fastly_waf/provider.tf
+++ google_fastly_waf/provider.tf
@@ -1,6 +1,4 @@
 # NGWAF SIGSCI
 provider "sigsci" {
-  corp     = var.ngwaf_corp
-  email    = var.ngwaf_email
   password = "not_a_real_password_set_for_terraform_validate" # pragma: allowlist secret
 }
