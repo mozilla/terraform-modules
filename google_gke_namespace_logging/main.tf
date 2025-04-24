mozilla/terraform-modules

commit 9a64e27f253986079de1696189ad011b71437703

fix(google_gke_namespace_logging): Prevent loss of logs when modifying log sink (#292)
* fix(google_gke_namespace_logging): Prevent loss of logs when modifying log sink

https://mozilla-hub.atlassian.net/browse/OPST-2378

When you set up a service in GCPv2 you can choose between Cloud Logging & BigQuery for storing logs.
The log sink is created in global-platform-admin, and the log sink destination is created in 
(dataservices|webservices)-infra. This split means that if you change the log sink from BigQuery to
Cloud Logging or vice versa you will lose logs written to the log sink until the log sink
destination has been created.

The fix is to always create both log sink destinations, and grant the unique writer identity
permissions to write to both.

* chore(docs): google_gke_namespace_logging/README.md

google_gke_namespace_logging/main.tf
@@ -9,18 +9,26 @@ locals {
   tenant_namespace = "${var.application}-${var.environment}"
 }
 
+moved {
+  from = google_logging_project_bucket_config.namespace[0]
+  to   = google_logging_project_bucket_config.namespace
+}
+
 resource "google_logging_project_bucket_config" "namespace" {
-  count            = var.log_destination == "bucket" ? 1 : 0
   project          = var.project
   location         = var.location
   bucket_id        = "gke-${local.tenant_namespace}-log-bucket"
   description      = "Log bucket for ${local.tenant_namespace}"
   retention_days   = var.retention_days
-  enable_analytics = var.log_analytics
+  enable_analytics = true
+}
+
+moved {
+  from = google_project_iam_member.logging_bucket_writer[0]
+  to   = google_project_iam_member.logging_bucket_writer
 }
 
 resource "google_project_iam_member" "logging_bucket_writer" {
-  count   = var.logging_writer_service_account_member != "" ? 1 : 0
   project = var.project
   role    = "roles/logging.bucketWriter"
   member  = var.logging_writer_service_account_member
@@ -32,8 +40,12 @@ resource "google_project_iam_member" "logging_bucket_writer" {
   }
 }
 
+moved {
+  from = google_bigquery_dataset.namespace[0]
+  to   = google_bigquery_dataset.namespace
+}
+
 resource "google_bigquery_dataset" "namespace" {
-  count         = var.log_destination == "bigquery" ? 1 : 0
   dataset_id    = replace("gke-${local.tenant_namespace}-log", "-", "_")
   friendly_name = "gke-${local.tenant_namespace}-log-dataset"
   description   = "Log dataset for ${local.tenant_namespace}"
@@ -43,16 +55,24 @@ resource "google_bigquery_dataset" "namespace" {
   location                        = "US"
 }
 
+moved {
+  from = google_bigquery_dataset_iam_member.logging_dataset_writer[0]
+  to   = google_bigquery_dataset_iam_member.logging_dataset_writer
+}
+
 resource "google_bigquery_dataset_iam_member" "logging_dataset_writer" {
-  count      = var.log_destination == "bigquery" ? 1 : 0
-  dataset_id = google_bigquery_dataset.namespace[0].dataset_id
+  dataset_id = google_bigquery_dataset.namespace.dataset_id
   role       = "roles/bigquery.dataEditor"
   member     = var.logging_writer_service_account_member
 }
 
+moved {
+  from = google_logging_linked_dataset.namespace_linked_dataset[0]
+  to   = google_logging_linked_dataset.namespace_linked_dataset
+}
+
 resource "google_logging_linked_dataset" "namespace_linked_dataset" {
-  count       = var.log_analytics ? 1 : 0
   link_id     = replace("gke-${local.tenant_namespace}-log-linked", "-", "_")
-  bucket      = google_logging_project_bucket_config.namespace[0].id
+  bucket      = google_logging_project_bucket_config.namespace.id
   description = "Linked Dataset for GKE Namespace Logging ${local.tenant_namespace}"
 }

google_gke_namespace_logging/outputs.tf
@@ -1,12 +1,12 @@
 output "logging_bucket_id" {
-  value = var.log_destination == "bucket" ? google_logging_project_bucket_config.namespace[0].id : null
+  value = google_logging_project_bucket_config.namespace.id
 }
 
 output "logging_bucket_linked_dataset_id" {
   description = "Log Analytics BigQuery dataset id"
-  value       = var.log_analytics ? google_logging_linked_dataset.namespace_linked_dataset[0].bigquery_dataset[0].dataset_id : null
+  value       = google_logging_linked_dataset.namespace_linked_dataset.bigquery_dataset[0].dataset_id
 }
 
 output "logging_dataset_id" {
-  value = var.log_destination == "bigquery" ? google_bigquery_dataset.namespace[0].dataset_id : null
+  value = google_bigquery_dataset.namespace.dataset_id
 }

google_gke_namespace_logging/variables.tf
@@ -29,15 +29,3 @@ variable "logging_writer_service_account_member" {
   type        = string
   description = "The unique_writer_identity service account that is provisioned when creating a Logging Sink"
 }
-
-variable "log_destination" {
-  type        = string
-  description = "Destination for tenant application logs. Can be bucket or bigquery"
-  default     = "bucket"
-}
-
-variable "log_analytics" {
-  type        = bool
-  description = "Enable log analytics for log bucket"
-  default     = false
-}

commit bced20bf837381160289cf6db1e2924c92abc8a1

fix(google_gke_namespace_logging): Allow passing empty string as service account
https://mozilla-hub.atlassian.net/browse/OPST-2378
Breakage from https://github.com/mozilla/terraform-modules/pull/292

Some tenants set `disable_shared_integrations: true` which prevents them from
creating a log sink with a unique service account. We should handle this case
by conditionally creating the IAM permissions if the service account is not ""

google_gke_namespace_logging/main.tf
@@ -23,12 +23,9 @@ resource "google_logging_project_bucket_config" "namespace" {
   enable_analytics = true
 }
 
-moved {
-  from = google_project_iam_member.logging_bucket_writer[0]
-  to   = google_project_iam_member.logging_bucket_writer
-}
-
 resource "google_project_iam_member" "logging_bucket_writer" {
+  count = var.logging_writer_service_account_member != "" ? 1 : 0
+
   project = var.project
   role    = "roles/logging.bucketWriter"
   member  = var.logging_writer_service_account_member
@@ -55,12 +52,9 @@ resource "google_bigquery_dataset" "namespace" {
   location                        = "US"
 }
 
-moved {
-  from = google_bigquery_dataset_iam_member.logging_dataset_writer[0]
-  to   = google_bigquery_dataset_iam_member.logging_dataset_writer
-}
-
 resource "google_bigquery_dataset_iam_member" "logging_dataset_writer" {
+  count = var.logging_writer_service_account_member != "" ? 1 : 0
+
   dataset_id = google_bigquery_dataset.namespace.dataset_id
   role       = "roles/bigquery.dataEditor"
   member     = var.logging_writer_service_account_member