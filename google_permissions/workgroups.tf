9354e0946cea555440429f2278cd2a6b5445a516
André Honeiser <ahoneiser@mozilla.com>
2025-05-06 14:45:54 +0200

    fix(google_permissions): fix mozilla_workgroup module source

--- google_permissions/workgroups.tf
+++ google_permissions/workgroups.tf
@@ -1,14 +1,14 @@
 module "admins_workgroup" {
-  source = "../mozilla_workgroup"
+  source = "github.com/mozilla/terraform-modules//mozilla_workgroup?ref=main"
   ids    = var.admin_ids
 }
 
 module "developers_workgroup" {
-  source = "../mozilla_workgroup"
+  source = "github.com/mozilla/terraform-modules//mozilla_workgroup?ref=main"
   ids    = var.developer_ids
 }
 
 module "viewers_workgroup" {
-  source = "../mozilla_workgroup"
+  source = "github.com/mozilla/terraform-modules//mozilla_workgroup?ref=main"
   ids    = var.viewer_ids
 }
