d8b2839db90ef3c28375a7fbc7f1b8b43855042d
André Honeiser <ahoneiser@mozilla.com>
2025-05-13 11:06:06 +0200

    Google certificate manager certificate map/update (#305)
    * feat(google_certificate_manager_ceritifcate_map): build resources in provider project by default
    
    * feat(google_certificate_manager_ceritifcate_map): add certmap resource as output
    
    * chore(docs): google_certificate_manager_certificate_map/README.md
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- google_certificate_manager_certificate_map/outputs.tf
+++ google_certificate_manager_certificate_map/outputs.tf
@@ -1,3 +1,7 @@
+output "certificate_map" {
+  value = google_certificate_manager_certificate_map.default
+}
+
 output "dns_authorizations" {
   value = flatten([for a in google_certificate_manager_dns_authorization.default : a.dns_resource_record])
 }
