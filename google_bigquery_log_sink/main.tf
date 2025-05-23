d19d49969ee5efee30cf2c2c1fbe1656b958e9aa
Austin Mitchell <amitchell@mozilla.com>
2025-05-23 10:20:08 -0700

    feat:add override vars to bq log sink module

--- google_bigquery_log_sink/main.tf
+++ google_bigquery_log_sink/main.tf
@@ -3,22 +3,22 @@ locals {
 }
 
 resource "google_bigquery_dataset" "self" {
-  dataset_id = "log_storage_${var.environment}"
+  dataset_id = var.dataset_id_override != "" ? var.dataset_id_override : "log_storage_${var.environment}"
 
   default_table_expiration_ms     = local.ninety_days_in_ms
-  default_partition_expiration_ms = local.ninety_days_in_ms
+  default_partition_expiration_ms = var.use_partitioned_tables == true ? local.ninety_days_in_ms : 0
   location                        = "US"
 }
 
 resource "google_logging_project_sink" "self" {
-  name        = "bigquery-log-storage-${var.environment}"
+  name        = var.sink_name_override != "" ? var.sink_name_override : "bigquery-log-storage-${var.environment}"
   destination = "bigquery.googleapis.com/${google_bigquery_dataset.self.id}"
 
   filter                 = var.log_filter
   unique_writer_identity = true
 
   bigquery_options {
-    use_partitioned_tables = true
+    use_partitioned_tables = var.use_partitioned_tables
   }
 }
 
