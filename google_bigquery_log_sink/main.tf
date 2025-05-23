15c994bc6de656515ce467ef95b96a98b4701e08
amitchell-moz <amitchell@mozilla.com>
2025-05-23 10:58:56 -0700

    Update google_bigquery_log_sink/main.tf
    Co-authored-by: whd <whd@users.noreply.github.com>

--- google_bigquery_log_sink/main.tf
+++ google_bigquery_log_sink/main.tf
@@ -6,7 +6,7 @@ resource "google_bigquery_dataset" "self" {
   dataset_id = var.dataset_id_override != "" ? var.dataset_id_override : "log_storage_${var.environment}"
 
   default_table_expiration_ms     = local.ninety_days_in_ms
-  default_partition_expiration_ms = var.use_partitioned_tables == true ? local.ninety_days_in_ms : 0
+  default_partition_expiration_ms = var.use_partitioned_tables ? local.ninety_days_in_ms : 0
   location                        = "US"
 }
 
