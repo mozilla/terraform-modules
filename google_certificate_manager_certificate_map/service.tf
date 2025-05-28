f15d8c2cd786ef2499db009f8efff0cb59b342cb
Eric Maydeck <emaydeck@mozilla.com>
2025-05-28 14:39:11 -0600

    chore(google_certificate_manager_certificate_map): no longer enable googleapi service

--- google_certificate_manager_certificate_map/service.tf
+++
@@ -1,7 +0,0 @@
-resource "google_project_service" "certificatemanager" {
-  project = var.shared_infra_project_id
-
-  service = "certificatemanager.googleapis.com"
-
-  disable_on_destroy = false
-}
