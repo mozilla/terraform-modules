8a8b213404afcdc5ae5fe429a84cb561384e1bfa
Rachael <rcrook@mozilla.com>
2025-06-05 14:18:55 -0500

    Updating condition for elastic/ec for the required provider (#310)
    * Updating elastic/ec version to 0.10.0 for the require provider
    
    * Updating elastic/ec required provider version to use a >= condition

--- google_psc_to_elastic/versions.tf
+++ google_psc_to_elastic/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     ec = {
       source  = "elastic/ec"
-      version = "~> 0.9.0"
+      version = ">= 0.9.0"
     }
     google = {
       source  = "hashicorp/google"
