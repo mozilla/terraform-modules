1c084283eda9902aec268e4e387813a8afdf702c
Rachael Crook <rcrook@mozilla.com>
2025-07-21 17:24:46 -0500

    Adding read endpoint and read endpoint port to outputs

--- google_redis/outputs.tf
+++ google_redis/outputs.tf
@@ -13,3 +13,11 @@ output "port" {
 output "persistence_iam_identity" {
   value = google_redis_instance.main.persistence_iam_identity
 }
+
+output "read_endpoint" {
+  value = google_redis_instance.main.read_endpoint
+}
+
+output "read_endpoint_port" {
+  value = google_redis_instance.main.read_endpoint_port
+}
