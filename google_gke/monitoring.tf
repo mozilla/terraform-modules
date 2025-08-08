c8de5cb05f25f6e2b65adf61e8e833d908f5791b
Jon Buckley <jbuckley@mozilla.com>
2025-08-08 12:42:59 -0400

    fix(google_gke): Use partition expiry for resource usage export
    MZCLD-795

--- google_gke/monitoring.tf
+++ google_gke/monitoring.tf
@@ -1,10 +1,10 @@
 resource "google_bigquery_dataset" "dataset" {
   count = var.create_resource_usage_export_dataset ? 1 : 0
 
-  dataset_id                  = replace(local.cluster_name, "-", "_")
-  friendly_name               = local.cluster_name
-  description                 = "Metrics shipped from ${local.cluster_name} GKE Cluster."
-  default_table_expiration_ms = 604800000 # 1 week
+  dataset_id                      = replace(local.cluster_name, "-", "_")
+  friendly_name                   = local.cluster_name
+  description                     = "Metrics shipped from ${local.cluster_name} GKE Cluster."
+  default_partition_expiration_ms = 604800000 # 1 week
 
   labels = local.labels
 
