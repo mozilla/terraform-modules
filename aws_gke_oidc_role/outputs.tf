c415a7fc7a1f5ff61bc8c70a0578fdd141c0fb20
Bheesham Persaud <bhee@mozilla.com>
2025-08-08 10:27:49 -0400

    feat(aws_gke_oidc_role): Output the role ARN and Name
    Jira: IAM-1735

--- aws_gke_oidc_role/outputs.tf
+++ aws_gke_oidc_role/outputs.tf
@@ -1,4 +1,9 @@
+output "role_name" {
+  description = "Name for the GKE-AWS connector role"
+  value       = module.iam_assumable_role_for_oidc.iam_role_name
+}
+
 output "role_arn" {
   description = "ARN for the GKE-AWS connector role"
-  value       = module.iam_assumable_role_for_oidc.iam_role_name
+  value       = module.iam_assumable_role_for_oidc.iam_role_arn
 }
