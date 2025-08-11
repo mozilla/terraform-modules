f889817430f36aee9b7eb52058d9bb8a27c48518
Bheesham Persaud <bhee@mozilla.com>
2025-08-11 16:31:25 -0400

    feat(google_certificate_manager_certificate_map): Add DNS Authorization for additional domains
    Jira: IAM-1735

--- google_certificate_manager_certificate_map/outputs.tf
+++ google_certificate_manager_certificate_map/outputs.tf
@@ -5,3 +5,7 @@ output "certificate_map" {
 output "dns_authorizations" {
   value = flatten([for a in google_certificate_manager_dns_authorization.default : a.dns_resource_record])
 }
+
+output "dns_authorizations_additional_domains" {
+  value = { for a in google_certificate_manager_dns_authorization.additional_domains : a.name => a.dns_resource_record }
+}
