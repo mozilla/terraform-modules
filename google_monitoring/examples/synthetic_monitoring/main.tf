c68a727fd7da635c0ef22e1d1264a113052ab02c
Basma1912 <basad@mozilla.com>
2025-08-14 17:33:57 -0400

    feat: add alerting policy for synthetic monitoring-adding-examples
    Signed-off-by: Basma1912 <basad@mozilla.com>

--- google_monitoring/examples/synthetic_monitoring/main.tf
+++ google_monitoring/examples/synthetic_monitoring/main.tf
@@ -1,3 +1,6 @@
+
+# Synthetic Monitoring checks without Alert Policy
+
 module "synthetic_monitors" {
   source     = "github.com/mozilla/terraform-modules//google_monitoring?ref=main"
   project_id = local.project_id
@@ -33,6 +36,103 @@ module "synthetic_monitors" {
       timeout     = 60
       secret_key  = "API_KEY"
       secret_name = "search-monitor-secret"
+    }
+  ]
+}
+
+# Synthetic Monitoring Checks with Alert Policy (minimal configuration)
+
+resource "google_monitoring_notification_channel" "dev_team_notification_channel" {
+  display_name = "Dev Team"
+  type         = "email"
+  labels = {
+    email_address = "dev_team+alerts@mydomain.com"
+  }
+  force_delete = false
+}
+
+module "synthetic_monitors" {
+  source     = "github.com/mozilla/terraform-modules//google_monitoring?ref=main"
+  project_id = local.project_id
+
+  synthetic_monitors = [
+    {
+      name                 = "login-check"
+      bucket_name          = "login-monitoring-bucket-unique-123"
+      object_name          = "login.zip"
+      object_source        = "src/login-check.zip"
+      function_name        = "login_function"
+      function_description = "test is a test"
+      function_location    = "us-central1"
+      entry_point          = "LoginCheck"
+      memory               = "256M"
+      runtime              = "nodejs20"
+      timeout              = 60
+      secret_key           = "API_KEY"
+      secret_name          = "login-monitor-secret"
+
+      alert_policy = {
+        enabled = true
+        notification_channels = [
+          google_monitoring_notification_channel.dev_team_notification_channel.id
+        ]
+      }
+    }
+  ]
+}
+
+# Synthetic Monitoring Checks with Alert Policy
+
+resource "google_monitoring_notification_channel" "dev_team_notification_channel" {
+  display_name = "Dev Team"
+  type         = "email"
+  labels = {
+    email_address = "dev_team+alerts@mydomain.com"
+  }
+  force_delete = false
+}
+
+module "synthetic_monitors" {
+  source     = "github.com/mozilla/terraform-modules//google_monitoring?ref=main"
+  project_id = local.project_id
+
+  synthetic_monitors = [
+    {
+      name                 = "login-check"
+      bucket_name          = "login-monitoring-bucket-unique-123"
+      object_name          = "login.zip"
+      object_source        = "src/login-check.zip"
+      function_name        = "login_function"
+      function_description = "test is a test"
+      function_location    = "us-central1"
+      entry_point          = "LoginCheck"
+      memory               = "256M"
+      runtime              = "nodejs20"
+      timeout              = 60
+      secret_key           = "API_KEY"
+      secret_name          = "login-monitor-secret"
+
+      alert_policy = {
+        enabled  = true
+        severity = local.realm == "prod" ? "CRITICAL" : "WARNING"
+        notification_channels = [
+          google_monitoring_notification_channel.dev_team_notification_channel.id
+        ]
+        alert_threshold_duration = "300s"
+        alignment_period         = "60s"
+        trigger_count            = 1
+        auto_close               = "7200s"
+        documentation_links = [
+          {
+            display_name = "Service Runbook"
+            url          = "https://mydomain.atlassian.net/wiki/spaces/FOLDER/pages/1234567/MYSERVICE+Runbook"
+          },
+          {
+            display_name = "Dev Team Slack Channel"
+            url          = "https://mydomain.slack.com/archives/ABCDEFG12345"
+          }
+        ]
+      }
     }
   ]
 }
