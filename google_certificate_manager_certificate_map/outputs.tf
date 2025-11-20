026591506ded8d358d82b3e97e124ac071c75a80
Eric Maydeck <emaydeck@mozilla.com>
2025-11-20 14:42:59 -0700

    test(google_certificate_manager_certificate_map): test different output

--- google_certificate_manager_certificate_map/outputs.tf
+++ google_certificate_manager_certificate_map/outputs.tf
@@ -3,5 +3,5 @@ output "certificate_map" {
 }
 
 output "dns_authorizations" {
-  value = flatten([for a in google_certificate_manager_dns_authorization.default : a.dns_resource_record])
+  value = flatten([for a in google_certificate_manager_dns_authorization.default : a])
 }
