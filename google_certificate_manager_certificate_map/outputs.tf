40cbd82f6397588093ef1d6cec086330dcf269ca
Eric Maydeck <emaydeck@mozilla.com>
2025-11-20 15:15:10 -0700

    feat(google_certificate_manager_certificate_map): added dns_authorizations_by_domain output

--- google_certificate_manager_certificate_map/outputs.tf
+++ google_certificate_manager_certificate_map/outputs.tf
@@ -3,5 +3,9 @@ output "certificate_map" {
 }
 
 output "dns_authorizations" {
-  value = { for dns_resource_record in flatten([for a in google_certificate_manager_dns_authorization.default : a.dns_resource_record]) : dns_record_resource.domain => dns_record_resource }
+  value = flatten([for a in google_certificate_manager_dns_authorization.default : a.dns_resource_record])
+}
+
+output "dns_authorizations_by_domain" {
+  value = { for b in flatten([for a in google_certificate_manager_dns_authorization.default : a]) : b.domain => b.dns_resource_record[0] }
 }
