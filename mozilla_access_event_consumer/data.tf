6db7b786ae4f5f89beec53d07d9f34655cb43a66
Wesley Dawson <whd@mozilla.com>
2026-02-12 10:44:19 -0800

    chore(mozilla_access_event_consumer): add FIXME

--- mozilla_access_event_consumer/data.tf
+++ mozilla_access_event_consumer/data.tf
@@ -1,3 +1,10 @@
+# Note that there is some longstanding technical debt associated with our IaC
+# configuration that means usage of remote state without SA impersonation
+# requires direct Atlantis/Spacelift access to be configured. Since these
+# remote state lookups are into platform-managed state where these grants
+# are in effect they should work, but in future we will likely clean this up to
+# require remote state lookups to use impersonation
+
 # Remote State Lookup for central topic (only when central_topic_id is not provided)
 data "terraform_remote_state" "global_shared" {
   count   = var.central_topic_id == null ? 1 : 0
