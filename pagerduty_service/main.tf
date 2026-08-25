41d5827422b93627ce0fa24b3c8d9d8a6cf65cf0
Jon Buckley <jbuckley@mozilla.com>
2026-08-25 15:05:14 -0400

    fix(pagerduty_service): Fix perma-diff when setting timeouts to 0
    MZCLD-3894

--- pagerduty_service/main.tf
+++ pagerduty_service/main.tf
@@ -1,8 +1,12 @@
 resource "pagerduty_service" "service" {
-  name                    = var.name
-  description             = var.description
-  auto_resolve_timeout    = var.auto_resolve_timeout
-  acknowledgement_timeout = var.acknowledgement_timeout
+  name        = var.name
+  description = var.description
+  # You thought that 0 is a pretty sensible developer interface to disable a timeout right?
+  # But what if you were a some kind of chaos lover and decided "The string 'null' is better"?
+  # Well then you'd be the Pagerduty Terraform developers ;(
+  # https://github.com/PagerDuty/terraform-provider-pagerduty/blob/960ed279bba2b96608bd04e9abedc140c98e782b/pagerduty/resource_pagerduty_service.go#L381-L399
+  auto_resolve_timeout    = var.auto_resolve_timeout == 0 ? "null" : var.auto_resolve_timeout
+  acknowledgement_timeout = var.acknowledgement_timeout == 0 ? "null" : var.auto_resolve_timeout
 
   escalation_policy = local.escalation_policy_id
   alert_creation    = "create_alerts_and_incidents"
