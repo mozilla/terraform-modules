2d13f6b35dc36a1e349820b5f7f14387e47b63f5
Jon Buckley <jbuckley@mozilla.com>
2026-08-25 16:11:28 -0400

    fix(pagerduty_service): Fix perma-diff when setting `acknowledgement_timeout` to 0

--- pagerduty_service/main.tf
+++ pagerduty_service/main.tf
@@ -6,7 +6,7 @@ resource "pagerduty_service" "service" {
   # Well then you'd be the Pagerduty Terraform developers ;(
   # https://github.com/PagerDuty/terraform-provider-pagerduty/blob/960ed279bba2b96608bd04e9abedc140c98e782b/pagerduty/resource_pagerduty_service.go#L381-L399
   auto_resolve_timeout    = var.auto_resolve_timeout == 0 ? "null" : var.auto_resolve_timeout
-  acknowledgement_timeout = var.acknowledgement_timeout == 0 ? "null" : var.auto_resolve_timeout
+  acknowledgement_timeout = var.acknowledgement_timeout == 0 ? "null" : var.acknowledgement_timeout
 
   escalation_policy = local.escalation_policy_id
   alert_creation    = "create_alerts_and_incidents"
